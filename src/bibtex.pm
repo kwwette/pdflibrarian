386d3cbaa8a182e6073d578b16fd835a8bbc54b2
Karl Wette <karl.wette@ligo.org>
2020-10-23 17:58:07 +1100

    src/bibtex.pm: minor edit

--- src/bibtex.pm
+++ src/bibtex.pm
@@ -45,7 +45,7 @@ our @EXPORT_OK = qw(bib_checksum read_bib_from_str read_bib_from_file read_bib_f
 # BibTeX database structure
 my $structure = new Text::BibTeX::Structure('Bib');
 foreach my $type ($structure->types()) {
-  my @required_fields = sort(unique_list($structure->required_fields($type), qw(keyword file title year)));
+  my @required_fields = sort(unique_list($structure->required_fields($type), qw(keyword title year file)));
   my @optional_fields = sort(unique_list($structure->optional_fields($type), qw(collaboration)));
   $structure->set_fields($type, \@required_fields, \@optional_fields);
 }
