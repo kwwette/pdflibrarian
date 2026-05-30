2843688bccd2f7b4aec4801ad87abe3deb7028e9
Karl Wette <karl.wette@anu.edu.au>
2026-05-30 16:51:13 +1000

    lib/App/PDFLibrarian/TitleAbbr.pm: initialise variable

--- lib/App/PDFLibrarian/TitleAbbr.pm
+++ lib/App/PDFLibrarian/TitleAbbr.pm
@@ -36,7 +36,7 @@ our @EXPORT_OK = qw(get_aas_macros abbr_iso4_title);
 my %aas_macros;
 
 my @iso4_word_abbr;
-my $iso4_title_abbr_cachefile;
+my $iso4_title_abbr_cachefile = undef;
 my %iso4_title_abbr_cache;
 my $iso4_title_abbr_cache_new = 0;
 
